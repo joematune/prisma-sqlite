ebe0456fddfb462ebd622bed176881b263465733
joematune <joematune@gmail.com>
2020-11-04 09:58:45 +0200

    Add only a query to main() in script.ts

--- script.ts
+++ script.ts
@@ -6,20 +6,6 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  const post = await prisma.post.create({
-    data: {
-      title: 'Joe writes his first post!',
-      content: 'Writing writing writing',
-      published: true,
-      author: {
-        connect: {
-          email: 'joe@matu.ne'
-        }
-      }
-    },
-  })
-  console.log(post)
-
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
   })
